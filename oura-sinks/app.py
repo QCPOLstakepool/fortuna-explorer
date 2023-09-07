db3709b451fbd0217253360d064af1c129e856a3
QCPOLstakepool <qcpolstakepool@gmail.com>
2023-09-06 22:24:32 -0400

    WIP: Use Oura

--- oura-sinks/app.py
+++ oura-sinks/app.py
@@ -12,14 +12,13 @@ app = Flask(__name__)
 @app.route("/api/tx", methods = ["POST"])
 def post_tx():
     connection = None
+    data = request.json
 
     try:
-        json = request.json
-
-        if "transaction" not in json:  # Not what we're looking for
+        if "transaction" not in data:  # Not what we're looking for
             return "", 200
 
-        output_contract = get_output_contract(json)
+        output_contract = get_output_contract(data)
 
         if output_contract is None:
             return "Not a mint transaction.", 200
@@ -27,7 +26,7 @@ def post_tx():
         if output_contract["inline_datum"]["plutus_data"]["fields"][0]["int"] == 0:
             return "Genesis block.", 200
 
-        miner_output = get_miner_output(json)
+        miner_output = get_miner_output(data)
 
         if miner_output is None:
             raise Exception("Miner output not found.")
@@ -42,15 +41,15 @@ def post_tx():
             output_contract["inline_datum"]["plutus_data"]["fields"][4]["int"],
             output_contract["inline_datum"]["plutus_data"]["fields"][5]["int"],
             miner_output["address"],
-            json["context"]["block_number"],
-            json["transaction"]["hash"],
+            data["context"]["block_number"],
+            data["transaction"]["hash"],
         ))
 
         connection.commit()
 
         return "", 200
     except Exception as err:
-        print(request.json)
+        print(data)
         print(err)
 
         return "", 500
@@ -59,16 +58,16 @@ def post_tx():
             connection.close()
 
 
-def get_output_contract(json):
-    for output in json["transaction"]["outputs"]:
+def get_output_contract(data):
+    for output in data["transaction"]["outputs"]:
         if output["address"] == "addr1wynelppvx0hdjp2tnc78pnt28veznqjecf9h3wy4edqajxsg7hwsc" and len(list(filter(lambda asset: asset["policy"] == "279f842c33eed9054b9e3c70cd6a3b32298259c24b78b895cb41d91a" and asset["asset"] == "6c6f72642074756e61", output["assets"]))) == 1:
             return output
 
     return None
 
 
-def get_miner_output(json):
-    for output in json["transaction"]["outputs"]:
+def get_miner_output(data):
+    for output in data["transaction"]["outputs"]:
         if len(list(filter(lambda asset: asset["policy"] == "279f842c33eed9054b9e3c70cd6a3b32298259c24b78b895cb41d91a" and asset["asset"] == "54554e41", output["assets"]))) == 1:
             return output
 
