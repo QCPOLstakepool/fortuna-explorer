de6ce900f9fec43cdc9522b556dd07149fb57033
QCPOLstakepool <qcpolstakepool@gmail.com>
2023-09-06 21:41:30 -0400

    WIP: Use Oura

--- oura-sinks/init.py
+++ oura-sinks/init.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         config = json.load(config_file)
 
     if os.path.exists(config["sqlite"]):
-        print("Database {} already exists!", config["sqlite"])
+        print("Database {} already exists!".format(config["sqlite"]))
     else:
         connection = sqlite3.connect(config["sqlite"])
         cursor = connection.cursor()
@@ -36,8 +36,8 @@ if __name__ == "__main__":
             )
         """)
 
-        cursor.execute("insert into block(number, leading_zeroes, difficulty, hash, epoch_time, posix_time, miner, cardano_block_no, cardano_tx_hash) values(0, 5, 65535, 'e4390b57fd759b5961107b931dca6d826cb2c272f0f711e266df48d0afc3a441', 0, 1693078039000, 'genesis', 9210120, 'f3d2e04e0391f7c95a1aeb6b3f35e33ffd6a060ac36a9ecb64af7f06ae0aa907')")
+        cursor.execute("insert into block(number, leading_zeroes, difficulty, hash, epoch_time, posix_time, miner, cardano_block_no, cardano_tx_hash) values(-1, 5, 65535, 'e4390b57fd759b5961107b931dca6d826cb2c272f0f711e266df48d0afc3a441', 0, 1693078039000, 'genesis', 9210120, 'f3d2e04e0391f7c95a1aeb6b3f35e33ffd6a060ac36a9ecb64af7f06ae0aa907')")
 
         connection.close()
 
-        print("Database {} initialized!", config["sqlite"])
+        print("Database {} initialized!".format(config["sqlite"]))
--- oura-sinks/tuna-policy-sink/app.py
+++ oura-sinks/tuna-policy-sink/app.py
@@ -10,7 +10,44 @@ app = Flask(__name__)
 
 @app.route("/api/tx", methods = ["POST"])
 def post_tx():
-    print("NEW REQUEST")
-    print(request.json)
+    try:
+        json = request.json
+
+        output_contract = get_output_contract(json)
+
+        if output_contract is None:
+            return "Outout to contract address not found.", 500
+
+        if output_contract["inline_datum"]["plutus_data"]["fields"][0]["int"] == 0:
+            return "Genesis block.", 200
+
+        miner_output = get_miner_output(json)
+
+        connection = sqlite3.connect(config["sqlite"])
+        cursor = connection.cursor()
+
+        cursor.execute("insert into block(number, leading_zeroes, difficulty, hash, epoch_time, posix_time, miner, cardano_block_no, cardano_tx_hash) values(?, ?, ?, ?, ?, ?, ?, ?, ?)", (
+            output_contract["inline_datum"]["plutus_data"]["fields"][0]["int"] - 1,
+            output_contract["inline_datum"]["plutus_data"]["fields"][2]["int"],
+            output_contract["inline_datum"]["plutus_data"]["fields"][3]["int"],
+            output_contract["inline_datum"]["plutus_data"]["fields"][1]["bytes"],
+            output_contract["inline_datum"]["plutus_data"]["fields"][4]["int"],
+            output_contract["inline_datum"]["plutus_data"]["fields"][5]["int"]
+
+        ))
+
+        return "", 200
+    except:
+        print(request.json)
+
+        return "", 500
 
 
+def get_output_contract(json):
+    for output in json["transaction"]["outputs"]:
+        if output["address"] == "addr1wynelppvx0hdjp2tnc78pnt28veznqjecf9h3wy4edqajxsg7hwsc":
+            return output
+
+    return None
+
+def get_miner_output(json):
